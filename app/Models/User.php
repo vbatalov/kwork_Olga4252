5d65b58f9174190ced70153348ec13145cb69cb7
vbatalov <vbatalov1996@gmail.com>
2024-02-16 11:37:34 +0700

    auto init user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,51 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected array $fillable = [
-        "cid",
+        "peer_id",
         "name",
         "surname",
         "role",
         "percent",
     ];
+
+
+    public User $user;
+    public string $peer_id;
+    public array $userInfo;
+
+    public function __construct(array $userInfo = null)
+    {
+        if ($userInfo != null) {
+            /** Информация о пользователя */
+            $this->userInfo = $userInfo;
+
+            /** Инициализация пользователя */
+            $this->peer_id = $userInfo['id'];
+            $this->init();
+        }
+
+    }
+
+    /**
+     * Инициализирует пользователя
+     * Создает, если нет в БД
+     * Обновляет данные
+     */
+    private function init(): void
+    {
+        /** Инициализация пользователя или создание нового */
+        $this->user = User::updateOrCreate(
+            [
+                "peer_id" => $this->peer_id,
+            ],
+            [
+                "name" => $this->userInfo['first_name'],
+                "surname" => $this->userInfo['last_name'],
+            ]
+        );
+
+
+    }
+
+
 }
